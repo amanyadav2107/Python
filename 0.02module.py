bbd3fb028e3a9bc35ce28bfb46fbe797fc9346fc
Aman Yadav <amanyadavchak@gmail.com>
2026-07-29 23:46:33 +0530

    Basic

---
+++ 0.02module.py
@@ -0,0 +1,15 @@
+# It is used to comment in python.
+"""
+So thank that was my program
+
+"""
+print('''
+   
+Twinkle, twinkle, little star,
+how I wonder what you are.
+Up above the world so high,
+like a diamond in the sky. 
+Twinkle, twinkle, little star,
+how I wonder what you are.
+When the blazing sun is set, and the grass with dew is wet. Then you show your little
+are.''')
